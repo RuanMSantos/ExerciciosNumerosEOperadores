2168649a824a2f554b9af832b50ea11949dcc108
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 04:42:52 -0300

    Finaliza exercício

---
+++ GrausCF/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("°C = ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = celsius * 1.8 + 32;
+
+Console.WriteLine($"{celsius}°C equivalem a {fahrenheit}°F");
