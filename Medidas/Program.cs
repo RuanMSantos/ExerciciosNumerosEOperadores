ae7e3d1e5bfd3a9affec5bdf82801deedab0dae2
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 03:56:15 -0300

    Finaliza exercício

---
+++ Medidas/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Entre com a medida (em metros): ");
+double medida = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("--- Equivalência ---");
+Console.WriteLine($"{medida * 100} cm");
+Console.WriteLine($"{medida} m");
+Console.WriteLine($"{medida / 1000} km");
