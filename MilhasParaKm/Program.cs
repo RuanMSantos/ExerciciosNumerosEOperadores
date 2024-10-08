d740775e92619a26833e82bde16320b97900e58b
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 03:39:53 -0300

    Finaliza exercício

---
+++ MilhasParaKm/Program.cs
@@ -0,0 +1,6 @@
+﻿const double km = 1.609;
+
+Console.Write("Entre com a medida (em milhas): ");
+double medidas = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"{medidas *= km:N3} Km");
