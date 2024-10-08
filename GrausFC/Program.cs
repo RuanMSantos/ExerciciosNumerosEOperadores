5b37ef82302d3e08fd37b2035f936c1461b54eb0
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 04:52:52 -0300

    Finaliza exercício

---
+++ GrausFC/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("°F = ");
+double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+double celsius = (fahrenheit - 32) / 1.8;
+
+Console.WriteLine($"{fahrenheit}°F equivalem a {celsius:N2}°C");
