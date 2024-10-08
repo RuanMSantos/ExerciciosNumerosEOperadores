b3dfc42214ea244f8546945aa564c109f44bda09
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 04:15:56 -0300

    Finaliza exercício

---
+++ AreaTrianguloRet/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Base..: ");
+double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double area = (baseTriangulo * altura) / 2;
+
+Console.WriteLine($"\nÁrea...: {area}");
