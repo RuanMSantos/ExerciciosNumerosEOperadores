fb95041cf51f62ce07e55b39fc3b53da28b24929
RuanMSantos <ruancauancauan@gmail.com>
2025-04-03 18:46:57 -0300

    Finaliza exercício

---
+++ VelocMedia/Program.cs
@@ -0,0 +1,22 @@
+﻿double distancia = 0, tempo = 0;
+
+Console.Clear();
+
+Console.WriteLine("-- Velocidade média --\n");
+
+Console.Write("Distância percorrida (m): ");
+string dist = Console.ReadLine()!;
+
+if (!double.TryParse(dist, out distancia)) return;
+
+Console.Write("Tempo gasto (s): ");
+string temp = Console.ReadLine()!;
+
+if (!double.TryParse(temp, out tempo)) return;
+
+Console.WriteLine();
+
+Console.WriteLine(VelocMedia(distancia, tempo));
+string VelocMedia(double a, double b) => $"Velocidade média: {a / b:N2} m/s";
+
+Console.WriteLine();
