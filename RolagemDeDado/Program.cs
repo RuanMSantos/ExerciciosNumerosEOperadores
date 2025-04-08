e5443a673d3c125e2efffb8a34be33f677c51c5f
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 13:14:29 -0300

    Finaliza exercício

---
+++ RolagemDeDado/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+Random dado = new Random();
+
+Console.WriteLine($"\nRolagem: {dado.Next(1, faces++)}\n\n");
