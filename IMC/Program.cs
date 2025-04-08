34aeb9678a28322cc6bd73a01ef08a2e0709ea65
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 03:44:30 -0300

    Finaliza exercício

---
+++ IMC/Program.cs
@@ -0,0 +1,16 @@
+﻿string Imc(double a, double b){
+    double c = a / Math.Pow(b, 2);
+    return $"IMC: {c:N1} kg/m²";
+}
+
+double peso = 0, altura = 0;
+
+Console.Clear();
+
+Console.Write("Altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\nPeso (kg)...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Imc(peso, altura));
