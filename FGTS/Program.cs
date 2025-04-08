ab4db502c251388911aa37b1f264f5f68cf530c4
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 04:20:01 -0300

    Finaliza exercício

---
+++ FGTS/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine();
+
+Console.Write("Salário (R$)..: ");
+decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine();
+
+decimal fgts = 8 * salario / 100;
+
+Console.WriteLine($"FGTS: R$ {fgts:N2}\n");
