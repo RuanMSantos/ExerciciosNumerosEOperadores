84293399377c8e984087f663eb151b6629f8c108
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 05:17:43 -0300

    Finaliza exercício

---
+++ JurosSimples/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+decimal capital, taxa, tempo;
+
+Console.WriteLine("Juros simples (j)\n");
+
+Console.Write("Capital (R$).......: ");
+capital = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Taxa de juros (%)..: ");
+taxa = Convert.ToDecimal(Console.ReadLine()) / 100;
+
+Console.Write("Tempo (meses)......: ");
+tempo = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine();
+
+decimal
+    juros = capital * taxa * tempo,
+    montante = capital + juros;
+
+Console.WriteLine($"Juros (R$).....: {juros}");
+Console.WriteLine($"Montante (R$)..: {montante}");
+
+Console.WriteLine();
