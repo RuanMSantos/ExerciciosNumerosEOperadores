7e44b65f452d2febe86aa34a6fdc604e1e76f02d
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 14:03:43 -0300

    Finaliza exercício

---
+++ TempoDownload/Program.cs
@@ -0,0 +1,15 @@
+﻿const int valorByte = 8;
+
+Console.Clear();
+
+Console.WriteLine("--- Tempo de Download ---\n");
+
+Console.Write("Tamanho do arquivo em MB........: ");
+double tamanho = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Velocidade da conexão em Mbps...: ");
+double velocidade = Convert.ToDouble(Console.ReadLine()) / valorByte;
+
+double tempo = tamanho / velocidade / 60;
+
+Console.WriteLine($"\nTempo estimado de download: {tempo:N1} minutos\n\n");
