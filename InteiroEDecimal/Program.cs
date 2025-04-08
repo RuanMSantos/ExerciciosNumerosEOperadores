138f4fe11897c832c8e0ce40a1ff9a320535d43f
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 06:23:03 -0300

    Finaliza exercício

---
+++ InteiroEDecimal/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Inteiro e Decimal ---\n");
+
+Console.Write("Digite um número: ");
+double numero = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+
+double resto = numero % 1;
+
+Console.WriteLine($"Parte inteira: {Convert.ToInt32(numero)}");
+Console.WriteLine($"Parte decimal: {resto:N4}");
+
+Console.WriteLine();
