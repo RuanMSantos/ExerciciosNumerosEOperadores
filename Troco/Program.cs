38c6e25906a8e1ee0a54ab47af449235e90a4880
RuanMSantos <ruancauancauan@gmail.com>
2025-04-08 22:36:40 -0300

    Finaliza exercício

---
+++ Troco/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Digite o valor da compra: ");
+double compra = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor pago.....: ");
+double pagamento = Convert.ToInt32(Console.ReadLine());
+
+double troco = Math.Abs(compra - pagamento);
+
+Console.WriteLine($"\nTroco: {troco}");
