2a11dedbdc749ababfb9d4bce15eb3113b52f91d
RuanMSantos <ruancauancauan@gmail.com>
2025-04-14 10:53:33 -0300

    Adc exercício

---
+++ DecomposicaoDecimal/Program.cs
@@ -0,0 +1,15 @@
+﻿int u, d, c;
+
+Console.Clear();
+Console.WriteLine("-- Decomposição Decimal --\n");
+
+Console.Write("Digite um número inteiro...: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+u = num % 10;
+d = num / 10 % 10;
+c = num / 100;
+
+Console.WriteLine($"O número {num} possui:\n{u} unidade(s)\n{d} dezena(s)\n{c} centena(s).");
